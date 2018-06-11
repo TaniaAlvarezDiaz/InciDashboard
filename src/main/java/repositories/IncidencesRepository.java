f70e2b1114cf89542955a48a2886b5db481d7e89
TaniaAlvarezDiaz <tani.alvarez.diaz@gmail.com>
2018-06-11 20:08:00 +0200

    creacion proyecto

---
+++ src/main/java/repositories/IncidencesRepository.java
@@ -0,0 +1,16 @@
+package inciDashboard_e5a.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import inciDashboard_e5a.model.Incidence;
+
+@Repository
+public interface IncidencesRepository extends CrudRepository<Incidence, Long> {
+
+    // @Query("SELECT i FROM Incidence i WHERE i.user.identificador = ?1")
+    // public Page<Incidence> findIncidences(Pageable page, String identificador);
+    //
+    // @Query("SELECT i FROM Incidence i WHERE i.id = ?1")
+    // public Incidence findIncidence(Long id);
+}
